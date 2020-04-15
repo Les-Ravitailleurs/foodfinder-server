0afac504c56deb0e1066ac5bf76a69433cc3dd85
nmalzieu <noe.malzieu@gmail.com>
2020-04-15 12:11:25 +0200

    Autolint

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const logger = require("../logger");
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, API_PORT } = require("../config");
-
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = require("../config");
 
 const buildUser = require("./models/user");
 
