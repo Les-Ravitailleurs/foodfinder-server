ea18c98f9295a79e4c2cdc3d5fd12bd51ba7d2ee
nmalzieu <noe.malzieu@gmail.com>
2020-04-21 19:32:39 +0200

    Finish first version

--- src/api/index.js
+++ src/api/index.js
@@ -18,58 +18,57 @@ const launchAPI = () => {
 
   // On prod, we want to serve
   // the app through express
-  // if (Config.ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "app-desktop")));
-
-  // }
+  if (Config.ENV === "production") {
+    app.use(express.static(path.join(__dirname, "..", "app-desktop")));
+  }
 
   app.use("/webhooks", webhooks);
   app.use("/api", express.json());
   app.use("/api", router);
 
-  // if (Config.ENV === "production") {
-  app.all("*", async (req, res) => {
-    let poolId = null;
-    let title = null;
-    const regex1 = new RegExp("/collecte/(.*?)/");
-    const regex2 = new RegExp("/collecte/(.*?)(\\?|$)");
-    if (req.url.startsWith("/collecte/")) {
-      const match1 = req.url.match(regex1);
-      if (match1) {
-        poolId = match1[1];
-      } else {
-        const match2 = req.url.match(regex2);
-        if (match2) {
-          poolId = match2[1];
+  if (Config.ENV === "production") {
+    app.all("*", async (req, res) => {
+      let poolId = null;
+      let title = null;
+      const regex1 = new RegExp("/collecte/(.*?)/");
+      const regex2 = new RegExp("/collecte/(.*?)(\\?|$)");
+      if (req.url.startsWith("/collecte/")) {
+        const match1 = req.url.match(regex1);
+        if (match1) {
+          poolId = match1[1];
+        } else {
+          const match2 = req.url.match(regex2);
+          if (match2) {
+            poolId = match2[1];
+          }
         }
       }
-    }
-    if (poolId) {
-      // Let's get title from pool name
-      const pool = await ServicePool.getPool(poolId);
-      title = `Aidez ${pool.creatorName} !`;
-    }
-    console.log(title);
+      if (poolId) {
+        // Let's get title from pool name
+        const pool = await ServicePool.getPool(poolId);
+        title = `Aidez ${pool.creatorName} !`;
+      }
+      console.log(title);
 
-    fs.readFile(
-      path.join(__dirname, "..", "app-desktop", "index.html"),
-      "utf8",
-      function read(err, data) {
-        if (err) {
-          res.status(500).send(err);
+      fs.readFile(
+        path.join(__dirname, "..", "app-desktop", "index.html"),
+        "utf8",
+        function read(err, data) {
+          if (err) {
+            res.status(500).send(err);
+          }
+          // if (title) {
+          //   // Let's replace title !
+          //   data = data.replace(
+          //     "<title>Les ravitailleurs</title>",
+          //     `<title>${title}</title>`
+          //   );
+          // }
+          res.status(200).send(data);
         }
-        // if (title) {
-        //   // Let's replace title !
-        //   data = data.replace(
-        //     "<title>Les ravitailleurs</title>",
-        //     `<title>${title}</title>`
-        //   );
-        // }
-        res.status(200).send(data);
-      }
-    );
-  });
-  // }
+      );
+    });
+  }
 
   app.listen(port, () => {
     logger.info(`API running on port ${port}`);
